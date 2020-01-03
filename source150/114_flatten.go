95a9dc05484b06e9d2786bd04efe92183c431b43
vito.chen <vitochan617@gmail.com>
2020-01-03 23:57:35 +0800

    fix 114

--- source150/114_flatten.go
+++ source150/114_flatten.go
@@ -29,7 +29,11 @@ Example:
  */
 
  /**
- 思路：按照先序遍历逻辑。可以借助栈，利用空间排序，再重新生成链表。
+ 思路：
+ 1. 找到i节点左子树的最大数值节点，即找到左子树的最右节点，记为preNode
+ 2. 将i节点的右子树放到preNode右子树
+ 3. 将i节点的左子树移动到右子树位置
+ 4. 递归i节点右子节点
   */
 
   /**
@@ -37,6 +41,20 @@ Example:
   内存：3MB
    */
 func flatten(root *TreeNode)  {
-
+	if root == nil {
+		return
+	}
+	if root.Left == nil {
+		flatten(root.Right)
+		return
+	}
+	preNode := root.Left
+	for preNode.Right != nil {
+		preNode = preNode.Right
+	}
+	preNode.Right = root.Right
+	root.Right = root.Left
+	root.Left = nil
+	flatten(root.Right)
 }
 
